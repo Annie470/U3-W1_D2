c83a750cdb42d3ab54fb11e437e11e7f619a367c
Anna De Lise <anna_delise@libero.it>
2025-07-22 15:11:04 +0200

    add

--- src/MyNav.jsx
+++ src/MyNav.jsx
@@ -1,11 +1,12 @@
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function CustomNav() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-warning">
       <Container>
         <Navbar.Brand href="#home">Zilla Lib</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
