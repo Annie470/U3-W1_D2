519eee2b54f436ed559a6873847d87d55de468a9
Anna De Lise <anna_delise@libero.it>
2025-07-23 16:07:24 +0200

    add

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 function Welcome() {
   return (
     <Container fluid className='bg-warning-subtle py-3 border-top border-black'>
-    <Alert variant="light" className='text-center border-1 border-warning w-50 mx-auto my-0'>
+    <Alert variant="light" className='text-center border-1 border-warning mx-auto my-0 w-75'>
       <Alert.Heading className='display-1'>CatZilla Library</Alert.Heading>
       <p>
      La tua e-library preferita!
