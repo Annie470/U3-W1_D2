38c89beff2129fdb24f9525efc10109e33ff6ec4
Anna De Lise <anna_delise@libero.it>
2025-07-24 17:02:23 +0200

    finish

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -23,11 +23,12 @@ return (
     <Card style={{ height: '100%' }} className={"d-flex flex-column" + (this.state.selected && "border-5 border-danger")}>
 <Card.Img variant="top" src={book.img} className="card-img-fixed" onClick={this.changeBorder}/>
       <Card.Body className='d-flex flex-column text-center justify-content-start'>
-        <Card.Title className="flex-grow-1">{book.title}</Card.Title>
+        <Card.Title>{book.title}</Card.Title>
+        <div className="flex-grow-1 d-flex flex-column justify-content-end">
         <Card.Text className="text-start">{book.price}€</Card.Text>
-        <Card.Text className="text-start">Categoria: {book.category}</Card.Text>
+        <Card.Text className="text-start">Categoria: {book.category}</Card.Text></div>
         </Card.Body>
-        {this.state.selected && <CommentArea className="flex-grow-2" id={book.asin}/>}
+        {this.state.selected && <CommentArea  className={'flex-grow-2'} id={book.asin}/>}
     </Card>
      
 </>)
